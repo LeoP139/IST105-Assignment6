5aa339a5ca24357ebfe144e1fcae186ef6968b67
LeoP139 <leoandrespadilla@gmail.com>
2025-06-30 20:08:44 -0700

    Urls

--- assignment6/bitwise/urls.py
+++ assignment6/bitwise/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.input_view, name='input'),
-    path('history/', views.history_view, name='history'),
+    path('', views.home, name='home'),
 ]
